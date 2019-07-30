32332a25d79edf3a20e7a6c943145141b21e7cdd
Rina Razumov <rinash3@gmail.com>
2019-07-30 09:55:52 -0400

    undignored build folder

---
+++ build/precache-manifest.33cab8eba05e7275538b4ca8aed62236.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40de7a81486f78f864a59600a179ebef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83ffae4d35431d2e6882",
+    "url": "/static/css/main.e6575b5d.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "83ffae4d35431d2e6882",
+    "url": "/static/js/main.ce0fda60.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
